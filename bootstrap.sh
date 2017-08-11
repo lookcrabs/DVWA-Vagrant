07f8866d665e5715fb60ffb4727475c272c8d537
SeaPaSulli <seapasulli@uchicago.edu>
2017-08-11 00:56:02 -0500

    added small section to set all passwords to the same value of your choice

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,26 +1,61 @@
 #!/bin/bash
+PHP_FPM_PATH_INI='/etc/php/7.0/fpm/php.ini'
+PHP_FPM_POOL_CONF='/etc/php/7.0/fpm/pool.d/www.conf'
+MYSQL_ROOT_PW='zxc098$'
+MYSQL_dvwa_user='dvwa_root'
+MYSQL_dvwa_password='greenblue#42'
+DVWA_admin_password='violet#90'
+recaptcha_public_key='u8392ihj32kl8hujalkshuil32'
+recaptcha_private_key='89ry8932873832lih32ilj32'
 
-sudo apt-get update
-sudo apt-get dist-upgrade -y
-add-apt-repository -y ppa:nginx/stable
-sudo apt-get install -y nginx mariadb-server mariadb-client php php-common php-cgi php-fpm php-gd php-cli php-pear php-mcrypt php-mysql php-gd git vim
-mysqladmin -u root password 'your_password_here'
-/etc/init.d/nginx stop
-/etc/init.d/php5-fpm stop
 
-sed -i 's/^;cgi.fix_pathinfo.*$/cgi.fix_pathinfo = 0/g' /etc/php/fpm/php.ini
+install_base() {
+    add-apt-repository -y ppa:nginx/stable
+    sudo apt-get update
+    sudo apt-get dist-upgrade -y
+    sudo apt-get install -y nginx mariadb-server mariadb-client php php-common php-cgi php-fpm php-gd php-cli php-pear php-mcrypt php-mysql php-gd git vim
+}
 
-## Settings for DVWA to be extra vuln :)
-sed -i 's/allow_url_include = Off/allow_url_include = On/g' /etc/php/7.0/fpm/php.ini
-sed -i 's/allow_url_fopen = Off/allow_url_fopen = On/g' /etc/php/7.0/fpm/php.ini
-sed -i 's/safe_mode = On/safe_mode = Off/g' /etc/php/7.0/fpm/php.ini
-echo "magic_quotes_gpc = Off" >> /etc/php/7.0/fpm/php.ini
-sed -i 's/display_errors = Off/display_errors = On/g' /etc/php/7.0/fpm/php.ini
+config_mysql(){
+    mysqladmin -u root password "${MYSQL_ROOT_PW}"
+##config the mysql config file for root so it doesn't prompt for password. Also sets pw in plain text for easy access.  
+## Don't forget to change the password here!! 
 
-sed -i 's/^;security.limit_extensions.*$/security.limit_extensions = .php .php3 .php4 .php5 .php7/g' /etc/php/7.0/fpm/pool.d/www.conf
-sed -i 's/^listen.owner.*$/listen.owner = www-data/g' /etc/php/7.0/fpm/pool.d/www.conf
-sed -i 's/^listen.group.*$/listen.group = www-data/g' /etc/php/7.0/fpm/pool.d/www.conf
-sed -i 's/^;listen.mode.*$/listen.mode = 0660/g' /etc/php/7.0/fpm/pool.d/www.conf
+cat <<EOF > /root/.my.cnf
+[client]
+user="root"
+password="${MYSQL_ROOT_PW}"
+EOF
+    mysql -BNe "drop database dvwa;"
+    mysql -BNe "CREATE DATABASE dvwa;"
+    mysql -BNe "GRANT ALL ON *.* TO '"${MYSQL_dvwa_user}"'@'localhost' IDENTIFIED BY '"${MYSQL_dvwa_password}"';"
+
+    service mysql restart
+}
+
+
+config_php(){
+    ##Config PHP FPM INI to disable some security settings
+    
+    sed -i 's/^;cgi.fix_pathinfo.*$/cgi.fix_pathinfo = 0/g' ${PHP_FPM_PATH_INI}
+    sed -i 's/allow_url_include = Off/allow_url_include = On/g' ${PHP_FPM_PATH_INI}
+    sed -i 's/allow_url_fopen = Off/allow_url_fopen = On/g' ${PHP_FPM_PATH_INI}
+    sed -i 's/safe_mode = On/safe_mode = Off/g' ${PHP_FPM_PATH_INI}
+    echo "magic_quotes_gpc = Off" >> ${PHP_FPM_PATH_INI}
+    sed -i 's/display_errors = Off/display_errors = On/g' ${PHP_FPM_PATH_INI}
+
+    ##explicitly set pool options (these are defaults in ubuntu 16.04 so i'm commenting them out. If they are not defaults for you try uncommenting these
+    #sed -i 's/^;security.limit_extensions.*$/security.limit_extensions = .php .php3 .php4 .php5 .php7/g' /etc/php/7.0/fpm/pool.d/www.conf
+    #sed -i 's/^listen.owner.*$/listen.owner = www-data/g' /etc/php/7.0/fpm/pool.d/www.conf
+    #sed -i 's/^listen.group.*$/listen.group = www-data/g' /etc/php/7.0/fpm/pool.d/www.conf
+    #sed -i 's/^;listen.mode.*$/listen.mode = 0660/g' /etc/php/7.0/fpm/pool.d/www.conf
+
+
+    systemctl restart php7.0-fpm
+}
+
+
+config_nginx(){
 
 cat << 'EOF' > /etc/nginx/sites-enabled/default
 server
@@ -45,30 +80,61 @@ server
 }
 EOF
 
-cat <<'EOF' > /root/.my.cnf
-[client]
-user="root"
-password="your_password_here"
-EOF
+    systemctl restart nginx
+}
 
-service mysql restart
-systemctl restart php7.0-fpm
-systemctl restart nginx
 
-mysql -BNe "CREATE DATABASE dvwa;"
-mysql -BNe "GRANT ALL ON *.* TO 'dvwa_root'@'localhost' IDENTIFIED BY 'your_password_here';"
-if [[ ! -d "/var/www/html" ]]; 
-then 
-      mkdir -p /var/www;
-      ln -s /usr/share/nginx/html /var/www/html;
-      chown -R www-data. /var/www/html;
-fi
+install_dvwa(){
 
-rm /var/www/html/*.html
+    if [[ ! -d "/var/www/html" ]]; 
+    then 
+          mkdir -p /var/www;
+          ln -s /usr/share/nginx/html /var/www/html;
+          chown -R www-data. /var/www/html;
+    fi
 
-cd /var/www/html && git clone https://github.com/RandomStorm/DVWA.git && chown -R www-data. ./ && mv ./DVWA/* . && cp config/config.inc.php.dist config/config.inc.php && chmod 777 ./hackable/uploads/; chmod 777 ./external/phpids/0.6/lib/IDS/tmp/phpids_log.txt
-sed -i '/db_user/ s/root/dvwa_root/' /var/www/html/config/config.inc.php
-sed -i '/db_password/ s/p@ssw0rd/your_password_here/'
-sed -i "/recaptcha_public_key/ s/''/'your_google_recaptcha_public_key_here'/" /var/www/html/config/config.inc.php
-sed -i "/recaptcha_private_key/ s/''/'your_google_recaptcha_private_key_here'/" /var/www/html/config/config.inc.php
+    cd /var/www/html
+    rm -rf /var/www/html/.*
+    rm -rf /var/www/html/*
+    git clone https://github.com/RandomStorm/DVWA.git ./
+    chown -R www-data. ./
+    cp config/config.inc.php.dist config/config.inc.php
+
+    ### chmod uploads and log file to be writable by nobody 
+    chmod 777 ./hackable/uploads/
+    chmod 777 ./external/phpids/0.6/lib/IDS/tmp/phpids_log.txt
+
+    ## change the values in the config to match our setup (these are what you need to update!
+    sed -i '/db_user/ s/root/'${MYSQL_dvwa_user}'/' /var/www/html/config/config.inc.php
+    sed -i '/db_password/ s/p@ssw0rd/'${MYSQL_dvwa_password}'/' /var/www/html/config/config.inc.php
+    sed -i "/recaptcha_public_key/ s/''/'"${recaptcha_public_key}"'/" /var/www/html/config/config.inc.php
+    sed -i "/recaptcha_private_key/ s/''/'"${recaptcha_private_key}"'/" /var/www/html/config/config.inc.php
+
+}
+
+
+update_mysql_user_pws(){
+## The mysql passwords are set via /usr/share/nginx/html/dvwa/includes/DBMS/MySQL.php. If you edit this every time they are reset it will reset to those. Otherwise you can do a sql update statement to update them all (they are just md5's of the string.
+# The issue is the users table doesn't get created until you click that button T_T to init.
+
+#mysql -BNe "UPDATE dvwa.users SET password = md5('YOUR_MYSQL_PW_HERE') WHERE user = 'admin';"
+#mysql -BNe "UPDATE dvwa.users SET password = md5('YOUR_MYSQL_PW_HERE') WHERE user = 'gordonb';"
+#mysql -BNe "UPDATE dvwa.users SET password = md5('YOUR_MYSQL_PW_HERE') WHERE user = '1337';"
+#mysql -BNe "UPDATE dvwa.users SET password = md5('YOUR_MYSQL_PW_HERE') WHERE user = 'pablo';"
+#mysql -BNe "UPDATE dvwa.users SET password = md5('YOUR_MYSQL_PW_HERE') WHERE user = 'smithy';"
+
+sed -i '/admin/ s/password/'${DVWA_admin_password}'/g'  /usr/share/nginx/html/dvwa/includes/DBMS/MySQL.php
+sed -i '/gordonb/ s/abc123/'${DVWA_admin_password}'/g'  /usr/share/nginx/html/dvwa/includes/DBMS/MySQL.php
+sed -i '/1337/ s/charley/'${DVWA_admin_password}'/g'  /usr/share/nginx/html/dvwa/includes/DBMS/MySQL.php
+sed -i '/pablo/ s/letmein/'${DVWA_admin_password}'/g'  /usr/share/nginx/html/dvwa/includes/DBMS/MySQL.php
+sed -i '/smithy/ s/password/'${DVWA_admin_password}'/g'  /usr/share/nginx/html/dvwa/includes/DBMS/MySQL.php
+}
+
+
+install_base
+config_mysql
+install_dvwa
+update_mysql_user_pws
+config_php
+config_nginx
 
